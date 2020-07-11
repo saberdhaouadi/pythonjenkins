935572fb24f697c56cff71ab81166b313d02d9d8
sdhaouadi <dhaouadis@gmail.com>
2020-07-11 01:56:09 +0100

    added some magic

--- file1.py
+++ file1.py
@@ -1,3 +1,9 @@
 print("welcome python trainer")
 print("#####################################################")
 print("Saber doing DevOps")
+
+print("let see some magic")
+for i in range(100):
+    print(i)
+
+
